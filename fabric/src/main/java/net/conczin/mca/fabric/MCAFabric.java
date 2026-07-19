d3e56f6da6f373171b0fdc5a3172b10f12b10f09
Ruslan <89846874+True-Ruslan@users.noreply.github.com>
2026-07-20 02:51:51 +0300

    fix: create LivingWorld config during Fabric startup

--- fabric/src/main/java/net/conczin/mca/fabric/MCAFabric.java
+++ fabric/src/main/java/net/conczin/mca/fabric/MCAFabric.java
@@ -7,6 +7,7 @@ import net.conczin.mca.entity.ai.MemoryModuleTypeMCA;
 import net.conczin.mca.entity.ai.SchedulesMCA;
 import net.conczin.mca.entity.ai.SensorsMCA;
 import net.conczin.mca.fabric.resources.*;
+import net.conczin.mca.livingworld.LivingWorldConfig;
 import net.conczin.mca.network.HandleablePayload;
 import net.conczin.mca.network.MessagesMCA;
 import net.conczin.mca.network.Network;
@@ -81,6 +82,8 @@ public final class MCAFabric implements ModInitializer {
 
     @Override
     public void onInitialize() {
+        LivingWorldConfig.getInstance();
+
         registerHelper(BuiltInRegistries.ITEM, ItemsMCA::registerItems);
         registerHelper(BuiltInRegistries.BLOCK, BlocksMCA::registerBlocks);
         registerHelper(BuiltInRegistries.SOUND_EVENT, SoundsMCA::registerSounds);
@@ -152,4 +155,3 @@ public final class MCAFabric implements ModInitializer {
         }
     }
 }
-
