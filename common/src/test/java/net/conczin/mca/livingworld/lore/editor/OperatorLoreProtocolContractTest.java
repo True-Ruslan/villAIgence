d159267b23def8e12baa16021cc35a0c41ff5086
Ruslan <89846874+True-Ruslan@users.noreply.github.com>
2026-08-01 19:17:33 +0300

    test: isolate operator lore request correlation policy

--- common/src/test/java/net/conczin/mca/livingworld/lore/editor/OperatorLoreProtocolContractTest.java
+++ common/src/test/java/net/conczin/mca/livingworld/lore/editor/OperatorLoreProtocolContractTest.java
@@ -1,36 +1,30 @@
 package net.conczin.mca.livingworld.lore.editor;
 
-import net.conczin.mca.livingworld.lore.OperatorLoreScope;
-import net.conczin.mca.network.c2s.OperatorLoreReadRequest;
-import net.conczin.mca.network.c2s.OperatorLoreWriteRequest;
-import net.conczin.mca.network.s2c.OperatorLoreResponse;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class OperatorLoreProtocolContractTest {
     @Test
-    void requestIdIsPreservedAcrossReadWriteAndResponseModels() {
-        OperatorLoreReadRequest read = new OperatorLoreReadRequest(41, "WORLD", -1);
-        OperatorLoreWriteRequest write = new OperatorLoreWriteRequest(
-                42,
-                "PLAYER",
-                -1,
-                "a".repeat(OperatorLoreEditorPolicy.REVISION_HEX_LENGTH),
-                "Lore"
-        );
-        OperatorLoreEditorResult result = new OperatorLoreEditorResult(
-                OperatorLoreScope.WORLD,
-                -1,
-                OperatorLoreEditorResult.Status.OK,
-                "Lore",
-                OperatorLoreEditorPolicy.revision("Lore")
-        );
-        OperatorLoreResponse response = new OperatorLoreResponse(43, result);
+    void requestIdsAdvanceMonotonicallyAndWrapToOne() {
+        assertEquals(1, OperatorLoreProtocolPolicy.nextRequestId(0));
+        assertEquals(42, OperatorLoreProtocolPolicy.nextRequestId(41));
+        assertEquals(1, OperatorLoreProtocolPolicy.nextRequestId(Integer.MAX_VALUE));
+    }
 
-        assertEquals(41, read.requestId());
-        assertEquals(42, write.requestId());
-        assertEquals(43, response.requestId());
-        assertEquals(result, response.toResult());
+    @Test
+    void onlyTheCurrentPositiveRequestMatchesAResponse() {
+        assertTrue(OperatorLoreProtocolPolicy.matches(41, 41));
+        assertFalse(OperatorLoreProtocolPolicy.matches(41, 40));
+        assertFalse(OperatorLoreProtocolPolicy.matches(0, 0));
+        assertFalse(OperatorLoreProtocolPolicy.matches(-1, -1));
+    }
+
+    @Test
+    void serverEchoDoesNotTransformTheCorrelationValue() {
+        assertEquals(43, OperatorLoreProtocolPolicy.echo(43));
+        assertEquals(Integer.MIN_VALUE, OperatorLoreProtocolPolicy.echo(Integer.MIN_VALUE));
     }
 }
