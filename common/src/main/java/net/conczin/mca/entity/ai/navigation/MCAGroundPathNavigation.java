3e1b02951782093f90305d14ddc6829b6d2f5e55
Ruslan <89846874+True-Ruslan@users.noreply.github.com>
2026-08-01 14:01:02 +0300

    fix: make MCA surface lookup water-tag aware

--- common/src/main/java/net/conczin/mca/entity/ai/navigation/MCAGroundPathNavigation.java
+++ common/src/main/java/net/conczin/mca/entity/ai/navigation/MCAGroundPathNavigation.java
@@ -1,11 +1,16 @@
 package net.conczin.mca.entity.ai.navigation;
 
+import net.minecraft.core.BlockPos;
+import net.minecraft.tags.FluidTags;
+import net.minecraft.util.Mth;
 import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.ai.navigation.GroundPathNavigation;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.pathfinder.PathFinder;
 
 public class MCAGroundPathNavigation extends GroundPathNavigation {
+    private static final int MAX_WATER_SURFACE_SCAN = 16;
+
     public MCAGroundPathNavigation(Mob mob, Level level) {
         super(mob, level);
     }
@@ -16,5 +21,23 @@ public class MCAGroundPathNavigation extends GroundPathNavigation {
         this.nodeEvaluator.setCanPassDoors(true);
         this.nodeEvaluator.setCanOpenDoors(true);
         return new PathFinder(this.nodeEvaluator, maxVisitedNodes);
+    }
+
+    @Override
+    public int getSurfaceY() {
+        int startY = this.mob.getBlockY();
+        int fallbackY = Mth.floor(this.mob.getY() + 0.5D);
+        BlockPos.MutableBlockPos pos = new BlockPos.MutableBlockPos(
+                this.mob.getX(), startY, this.mob.getZ()
+        );
+
+        return NavigationWaterSurfacePolicy.findSurfaceY(
+                this.mob.isInWater(),
+                this.canFloat(),
+                startY,
+                fallbackY,
+                y -> this.level.getFluidState(pos.setY(y)).is(FluidTags.WATER),
+                MAX_WATER_SURFACE_SCAN
+        );
     }
 }
