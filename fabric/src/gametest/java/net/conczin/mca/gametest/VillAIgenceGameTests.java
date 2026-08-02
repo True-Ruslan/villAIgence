713ab4cfee4092651811073973e44f6013b4a1c6
Ruslan <89846874+True-Ruslan@users.noreply.github.com>
2026-08-03 02:08:48 +0300

    test: use Minecraft 1.21.1 GameTest annotation

--- fabric/src/gametest/java/net/conczin/mca/gametest/VillAIgenceGameTests.java
+++ fabric/src/gametest/java/net/conczin/mca/gametest/VillAIgenceGameTests.java
@@ -1,10 +1,11 @@
 package net.conczin.mca.gametest;
 
-import net.fabricmc.fabric.api.gametest.v1.GameTest;
+import net.fabricmc.fabric.api.gametest.v1.FabricGameTest;
+import net.minecraft.gametest.framework.GameTest;
 import net.minecraft.gametest.framework.GameTestHelper;
 
-public final class VillAIgenceGameTests {
-    @GameTest
+public final class VillAIgenceGameTests implements FabricGameTest {
+    @GameTest(template = FabricGameTest.EMPTY_STRUCTURE)
     public void serverGameTestHarnessIsActive(GameTestHelper helper) {
         throw new AssertionError("RED: Fabric server GameTest harness is active");
     }
