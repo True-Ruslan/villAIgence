f73cb0f5cfd70586c8753d98fcb893ed0a658823
Ruslan <89846874+True-Ruslan@users.noreply.github.com>
2026-08-06 00:09:11 +0300

    test: define move controller bridge contract

--- fabric/src/gametest/java/net/conczin/mca/gametest/accessor/MobMoveControlAccessor.java
+++ fabric/src/gametest/java/net/conczin/mca/gametest/accessor/MobMoveControlAccessor.java
@@ -1,13 +1,10 @@
 package net.conczin.mca.gametest.accessor;
 
-import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.ai.control.MoveControl;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.gen.Accessor;
 
-/** Test-only accessor for reproducing a vehicle-owned active controller replacement. */
-@Mixin(Mob.class)
+/** Test-only bridge for reproducing a vehicle-owned active controller replacement. */
 public interface MobMoveControlAccessor {
-    @Accessor("moveControl")
     void mca$setActiveMoveControl(MoveControl replacement);
+
+    MoveControl mca$getActiveMoveControl();
 }
